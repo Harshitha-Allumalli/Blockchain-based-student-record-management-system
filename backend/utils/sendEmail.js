cb436e08a4ce909d13a707e69a27d1a3fc61babe
Harshitha-Allumalli <harshithaallumalli004@gmail.com>
2026-07-28 20:16:13 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -33,7 +33,7 @@ const sendEmail = async (options) => {
         console.log("✅ SMTP connection verified successfully.");
 
         const mailOptions = {
-            from: `"BlockEdu" <${process.env.SMTP_USER}>`,
+            from: '"BlockEdu" <harshithaallumalli004@gmail.com>',
             to: options.email,
             subject: options.subject,
             text: options.message,
